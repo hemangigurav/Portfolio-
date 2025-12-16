3e6c11a7fc2fc8100685f1bf5024b0ab87bb4bb4
Hemangi Gurav <121420438+hemangigurav@users.noreply.github.com>
2025-12-16 11:12:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+// HEADER TOGGLE
+
+let MenuBtn = document.getElementById('MenuBtn');
+
+MenuBtn.addEventListener('click', function(e) {
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    this.classList.toggle('fa-xmark')
+})
+
+
+// TYPING EFFECT
+
+let typed = new Typed('.auto-input',{
+    strings :['Full Stack Developer !','Front-End Developer !', 'Back-End Developer !', 'Python Developer !'],
+    typeSpeed: 120,
+    backSpeed: 50,
+    backDelay: 2000,
+    loop: true,
+})
+
+
+// ACTIVE LINK STATE ON SCROLL
+
+
+// GET ALL LINKS
+let navLinks = document.querySelectorAll('nav ul li a');
+// console.log(navLinks);
+
+
+// GET ALL SECTIONS
+let sections = document.querySelectorAll('section');
+//console.log(sections);
+
+
+window.addEventListener('scroll', function(){
+    const scrollPos = window.scrollY + 20
+    sections.forEach(section =>{
+        if(scrollPos > section.offsetTop && scrollPos < (section.offsetTop + section.offsetHeight)){
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                if(section.getAttribute('id') === link.getAttribute('href').substring(1)){
+                    link.classList.add('active')
+                }
+            });
+        } 
+     
+    });
+});
